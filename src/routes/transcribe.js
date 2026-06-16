35f7b1fc935c4e3c9aeb0b07f1fb3f3f2cba4d34
AryaBotz <87101138+AryaBotz@users.noreply.github.com>
2026-06-16 14:40:58 +0700

    Update transcribe.js

--- src/routes/transcribe.js
+++ src/routes/transcribe.js
@@ -1,23 +1,21 @@
 import express from "express";
 import { writeFileSync } from "fs";
 import { convertToWav } from "../utils/convertWav.js";
-import { runWhisper } from "../utils/runWhisper.js";
+import { runGroq } from "../utils/runGroq.js";
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
     try {
+
         const rawPath = "/tmp/audio.raw";
         const wavPath = "/tmp/audio.wav";
 
-        // ESP32 kirim PCM raw
         writeFileSync(rawPath, req.body);
 
-        // convert ke WAV
         await convertToWav(rawPath, wavPath);
 
-        // STT
-        const text = await runWhisper(wavPath);
+        const text = await runGroq(wavPath);
 
         res.json({ text });
 
